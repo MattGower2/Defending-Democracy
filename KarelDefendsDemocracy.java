dfb32905464c0799c6aa27c5b7d3d8167f8ea007
MattGower2 <78578775+MattGower2@users.noreply.github.com>
2021-02-10 19:56:22 -0600

    DefendingDemocracy

---
+++ KarelDefendsDemocracy.java
@@ -0,0 +1,45 @@
+package week1;
+import stanford.karel.*;
+
+public class KarelDefendsDemocracy extends SuperKarel {
+
+	public void run() {
+		move ();
+		move ();
+		move ();
+		move ();
+		move ();
+		turnLeft ();
+		move ();
+		pickBeeper ();
+		turnLeft ();
+		turnLeft ();
+		move ();
+		turnLeft ();
+		move ();
+		move ();
+		move ();
+		move ();
+		turnLeft ();
+		move ();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		pickBeeper();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move ();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move ();
+		
+				
+	}  
+	
+	
+					
+}   
